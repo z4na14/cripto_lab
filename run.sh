9c2d4dd38b696293047dfff0c7ada79a85805424
Jorge Adrian Saghin Dudulea <zanajorgesaghin@gmail.com>
2025-10-09 11:59:12 +0200

    update scripts

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
-docker compose up -d ./Docker
-rm -r ./Docker/config/www
-ln -s $PWD/src/site/ ./Docker/config/www
+#!/bin/bash
+
+docker compose -f ./Docker/docker-compose.yml down
+rm -rf ./Docker/config/
+mkdir -p ./Docker/config/www
+cp -r ./src/site/* ./Docker/config/www
+docker compose -f ./Docker/docker-compose.yml up -d
---
+++ update.sh
@@ -0,0 +1,2 @@
+rm -rf ./Docker/config/www/*
+cp -r ./src/site/* ./Docker/config/www
