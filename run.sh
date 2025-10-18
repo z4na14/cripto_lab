ad87f9440a5866c563dfd1721e78fa023e788674
Jorge Adrian Saghin Dudulea <zanajorgesaghin@gmail.com>
2025-10-18 19:31:42 +0200

    nose que cojones he hecho lol

--- run.sh
+++ run.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 docker compose -f ./Docker/docker-compose.yml down
-rm -rf ./Docker/config/
-mkdir -p ./Docker/config/www
-cp -r ./src/site/* ./Docker/config/www
 docker compose -f ./Docker/docker-compose.yml up -d
--- update.sh
+++
@@ -1,2 +0,0 @@
-rm -rf ./Docker/config/www/*
-cp -r ./src/site/* ./Docker/config/www
